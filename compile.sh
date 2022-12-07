acea639f6b0a4cf9f4bbbd7625558be0522ae54a
davidjdoherty <davidjdoherty@gmail.com>
2022-12-07 08:08:18 -0500

    Removing gradle folder, not needed

--- compile.sh
+++ compile.sh
@@ -3,3 +3,5 @@ mkdir  ledger-models-java/generated-src
 cd ledger-models-protos
 echo "Following command won't work when run as script, need to research that"
 protoc -I . --java_out=../ledger-models-java/generated-src **/*proto
+
+echo "TODO put rust protoc gen in here"
