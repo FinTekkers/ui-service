b92418f725d3d00133131e68899f2f2b3d3ea6fb
samiam376 <ssheridan211@gmail.com>
2023-01-11 22:16:06 -0800

    fix rust gen

--- ledger-models-rust/portfolio.rs
+++ ledger-models-rust/portfolio.rs
@@ -1,3 +1,4 @@
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct PortfolioProto {
     /// 1-4 reserved
@@ -15,6 +16,7 @@ pub struct PortfolioProto {
     #[prost(string, tag = "10")]
     pub portfolio_name: ::prost::alloc::string::String,
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct QueryPortfolioRequestProto {
     #[prost(string, tag = "1")]
@@ -28,6 +30,7 @@ pub struct QueryPortfolioRequestProto {
         super::position::PositionFilterProto,
     >,
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CreatePortfolioRequestProto {
     #[prost(string, tag = "1")]
@@ -37,6 +40,7 @@ pub struct CreatePortfolioRequestProto {
     #[prost(message, optional, tag = "20")]
     pub create_portfolio_input: ::core::option::Option<PortfolioProto>,
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CreatePortfolioResponseProto {
     #[prost(string, tag = "1")]
@@ -48,6 +52,7 @@ pub struct CreatePortfolioResponseProto {
     #[prost(message, repeated, tag = "30")]
     pub portfolio_response: ::prost::alloc::vec::Vec<PortfolioProto>,
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct QueryPortfolioResponseProto {
     #[prost(string, tag = "1")]
