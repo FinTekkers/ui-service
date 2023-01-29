239a215cc869efdbeeef6fc98f32c298c68d723f
davidjdoherty <davidjdoherty@gmail.com>
2023-01-29 11:13:57 -0500

    Adding python generated code

---
+++ ledger-models-python/build_generate_init_files.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+
+root = './src'
+paths = os.listdir(root)
+
+ignore_folders = [
+        "{}/{}".format(root,'.DS_Store'),
+        "{}/{}".format(root,'__pycache__'),
+        "{}/{}".format(root,'fintekkers_ledger_models.egg-info'),
+    ]
+
+def create_init_files(root:str):
+    paths = os.listdir(root)
+    for path in paths:
+        path = "{}/{}".format(root,path)
+
+        if os.path.isdir(path) and path not in ignore_folders:
+        # if p.is_dir():# and p.__str__ not in ignore_folders:
+            init_file = "{}/__init__.py".format(path)
+            if not os.path.exists(init_file):
+                f= open(init_file,"w+") 
+                f.flush()
+                f.close()
+                print("Created {}".format(init_file))
+
+            create_init_files(path)
+        elif path in ignore_folders:
+            print("Ignoring folder as its in the ignore list: {}".format(path))
+        # elif os.path.isdir(path):
+        #     print("Folder already has an init file")
+
+
+
+create_init_files(root)
