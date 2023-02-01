60510044b5af56fa387567dfba7e70a27ea6e659
davidjdoherty <davidjdoherty@gmail.com>
2023-01-31 22:10:54 -0500

    Adding rust test

--- ledger-models-rust/lib.rs
+++ ledger-models-rust/lib.rs
@@ -81,3 +81,34 @@ pub mod fintekkers {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use prost_types::Timestamp;
+
+    use crate::fintekkers;
+
+    #[test]
+    fn it_works() {
+
+        let now_timestamp = Timestamp::default();
+        let now_wrapped_timestap = Some(now_timestamp);
+
+        let as_of_timestamp = fintekkers::models::util::LocalTimestampProto {
+            time_zone: String::from("America/New_York"),
+            timestamp: now_wrapped_timestap
+        };
+
+        let portfolio = fintekkers::models::portfolio::PortfolioProto { 
+            as_of:  Some(as_of_timestamp),
+            object_class: String::from("Portfolio"),
+            version: String::from("0.0.1"),
+            portfolio_name: String::from("PortfolioName"),
+            is_link: false,
+            uuid: None
+
+        };
+
+        assert_eq!(portfolio.portfolio_name, String::from("PortfolioName"))
+    }
+}
