8e740cb9e622ee736af0bac606cc8ae8ead2d91e
samiam376 <ssheridan211@gmail.com>
2023-02-12 21:51:20 -0800

    workin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,28 @@
 <script>
-  import "../app.css";
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+		SkipToContent,
+		Content
+	} from 'carbon-components-svelte';
+
+	import 'carbon-components-svelte/css/white.css';
+
+	let isSideNavOpen = false;
 </script>
 
-<nav class=" bg-slate-300  bg-opacity-40">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 items-center justify-between">
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div class="flex flex-shrink-0 items-center font-bold text-md">
-            <a href="/" class="text-black-100 px-3 py-2 rounded-md text-lg font-medium">FNTKR</a>
-        </div>
-        </div>
-      </div>
-    </div>
-  </nav>
-  <slot/>
+<Header company="FNTKR" platformName="Analytics" bind:isSideNavOpen href="/">
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+	<HeaderNav>
+		<HeaderNavItem href="/valuation" text="Valuation" />
+		<HeaderNavItem href="/" text="Portfolios" />
+		<HeaderNavItem href="/" text="Securities" />
+	</HeaderNav>
+</Header>
+
+<Content>
+	<slot />
+</Content>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,49 @@
-Hello
+<script>
+	import { Tile, Grid, Row, Column, AspectRatio } from 'carbon-components-svelte';
+</script>
+
+<Grid padding>
+	<Row>
+		<Column>
+			<h1>Your New Financial Platform</h1>
+		</Column>
+	</Row>
+	<Row padding>
+		<Column padding aspectRatio="2x1">
+			<Tile>
+				<div class="div-content">
+					<h2>Language Agnostic</h2>
+					<p>
+						All API's based on ProtoBuff's, with bindings for Rust, Typescript/Javascript, Java,
+						Python, Go, and C++.
+					</p>
+				</div>
+			</Tile>
+		</Column>
+		<Column padding aspectRatio="2x1">
+			<Tile>
+				<div class="div-content">
+					<h2>Cloud Native</h2>
+					<p>Built to be cloud agnostic - hosted on AWS</p>
+				</div>
+			</Tile>
+		</Column>
+		<Column padding aspectRatio="2x1">
+			<Tile>
+				<div class="div-content">
+					<h2>Performant</h2>
+					<p>Core services are written in Rust for speed and memory safety.</p>
+				</div>
+			</Tile>
+		</Column>
+	</Row>
+</Grid>
+
+<style>
+	.div-content {
+		min-height: 100px;
+	}
+	h2 {
+		padding-bottom: 5px;
+	}
+</style>
