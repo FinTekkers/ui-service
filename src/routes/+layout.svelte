3863f893c13ac8589c12761c3279ccea8d108763
samiam376 <ssheridan211@gmail.com>
2023-02-21 15:05:17 -0800

    log out log in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,24 @@
 		HeaderNav,
 		HeaderNavItem,
 		SkipToContent,
-		Content
+		Content,
+		Tile,
+		Button,
+		HeaderUtilities,
+		HeaderSearch,
+		HeaderAction,
+		HeaderPanelLink
 	} from 'carbon-components-svelte';
+
+	import UserAvatarFilledAlt from 'carbon-icons-svelte/lib/UserAvatarFilledAlt.svelte';
 
 	import 'carbon-components-svelte/css/white.css';
 
 	let isSideNavOpen = false;
+	let loggedIn = true;
 </script>
 
-<Header company="FNTKR" platformName="Analytics" bind:isSideNavOpen href="/">
+<Header company="FNTKR" platformName="Ledger Modeling" bind:isSideNavOpen href="/">
 	<svelte:fragment slot="skip-to-content">
 		<SkipToContent />
 	</svelte:fragment>
@@ -21,8 +30,33 @@
 		<HeaderNavItem href="/" text="Portfolios" />
 		<HeaderNavItem href="/" text="Securities" />
 	</HeaderNav>
+	<HeaderUtilities>
+		<HeaderSearch />
+		<HeaderAction icon={UserAvatarFilledAlt}>
+			<HeaderPanelLink
+				on:click={() => {
+					loggedIn = !loggedIn;
+				}}
+				>{#if loggedIn}
+					Log Out
+				{:else}
+					Log In
+				{/if}
+			</HeaderPanelLink>
+		</HeaderAction>
+	</HeaderUtilities>
 </Header>
 
 <Content>
 	<slot />
 </Content>
+
+<style>
+	.centered {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		min-height: 85vh;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,48 +2,57 @@
 	import { Tile, Grid, Row, Column, AspectRatio } from 'carbon-components-svelte';
 </script>
 
-<Grid padding>
-	<Row>
-		<Column>
-			<h1>Your New Financial Platform</h1>
-		</Column>
-	</Row>
-	<Row padding>
-		<Column padding aspectRatio="2x1">
-			<Tile>
-				<div class="div-content">
-					<h2>Language Agnostic</h2>
-					<p>
-						All API's based on ProtoBuff's, with bindings for Rust, Typescript/Javascript, Java,
-						Python, Go, and C++.
-					</p>
-				</div>
-			</Tile>
-		</Column>
-		<Column padding aspectRatio="2x1">
-			<Tile>
-				<div class="div-content">
-					<h2>Cloud Native</h2>
-					<p>Built to be cloud agnostic - hosted on AWS</p>
-				</div>
-			</Tile>
-		</Column>
-		<Column padding aspectRatio="2x1">
-			<Tile>
-				<div class="div-content">
-					<h2>Performant</h2>
-					<p>Core services are written in Rust for speed and memory safety.</p>
-				</div>
-			</Tile>
-		</Column>
-	</Row>
-</Grid>
+<h1>Your New Financial Platform</h1>
+
+<div class="cards">
+	<div class="card">
+		<Tile>
+			<div class="div-content">
+				<h2>Language Agnostic</h2>
+				<p>
+					All API's are based on ProtoBuff's, with out of the box bindings for Rust,
+					Typescript/Javascript, Java, Python, Go, and C++.
+				</p>
+			</div>
+		</Tile>
+	</div>
+	<div class="card">
+		<Tile>
+			<div class="div-content">
+				<h2>Cloud Native</h2>
+				<p>Built to be cloud agnostic - hosted on AWS.</p>
+			</div>
+		</Tile>
+	</div>
+	<div class="card">
+		<Tile>
+			<div class="div-content">
+				<h2>Performant</h2>
+				<p>
+					Core services are written in Rust for speed and memory safety. Our UI's are built with
+					Svelte to keep page loads snappy.
+				</p>
+			</div>
+		</Tile>
+	</div>
+</div>
 
 <style>
+	h1 {
+		padding-bottom: 2rem;
+	}
 	.div-content {
-		min-height: 100px;
+		min-height: 125px;
 	}
 	h2 {
-		padding-bottom: 5px;
+		padding-bottom: 1rem;
+	}
+	.cards {
+		display: flex;
+		gap: 2rem;
+	}
+	.card {
+		flex: 1;
+		flex-basis: 100%;
 	}
 </style>
