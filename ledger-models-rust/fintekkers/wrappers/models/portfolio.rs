cb7536fc2c26279e80abf39da93123aef5f36ab7
davidjdoherty <dado0583@gmail.com>
2023-03-17 15:26:31 -0400

    Adding portfolio builder

--- ledger-models-rust/fintekkers/wrappers/models/portfolio.rs
+++ ledger-models-rust/fintekkers/wrappers/models/portfolio.rs
@@ -1,4 +1,7 @@
 use crate::fintekkers::models::portfolio::PortfolioProto;
+use crate::fintekkers::models::util::{LocalTimestampProto, UuidProto};
+use crate::fintekkers::wrappers::models::utils::datetime::LocalTimestampWrapper;
+use crate::fintekkers::wrappers::models::utils::uuid_wrapper::UUIDWrapper;
 
 pub struct PortfolioWrapper {
     proto: PortfolioProto
@@ -22,6 +25,73 @@ impl PortfolioWrapper {
     }
 }
 
+pub struct PortfolioProtoBuilder {
+    object_class: String,
+    version: String,
+    uuid: UuidProto,
+    as_of: LocalTimestampProto,
+    is_link: bool,
+    portfolio_name: String,
+}
+
+impl PortfolioProtoBuilder {
+    pub fn new() -> PortfolioProtoBuilder {
+        let uuid = UUIDWrapper::new_random();
+        let uuid_str = uuid.to_string();
+
+        PortfolioProtoBuilder {
+            object_class: "Portfolio".to_string(),
+            version: "0.0.1".to_string(),
+            uuid: uuid.into(),
+            as_of: LocalTimestampWrapper::now().into(),
+            is_link: false,
+            portfolio_name: uuid_str,
+        }
+    }
+
+    pub fn object_class(mut self, object_class: String) -> PortfolioProtoBuilder {
+        self.object_class = object_class;
+        self
+    }
+
+    pub fn version(mut self, version: String) -> PortfolioProtoBuilder {
+        self.version = version;
+        self
+    }
+
+    pub fn uuid(mut self, uuid: UuidProto) -> PortfolioProtoBuilder {
+        self.uuid = uuid;
+        self
+    }
+
+    pub fn as_of(mut self, as_of: LocalTimestampProto) -> PortfolioProtoBuilder {
+        self.as_of = as_of;
+        self
+    }
+
+    pub fn is_link(mut self, is_link: bool) -> PortfolioProtoBuilder {
+        self.is_link = is_link;
+        self
+    }
+
+    pub fn portfolio_name(mut self, portfolio_name: String) -> PortfolioProtoBuilder {
+        self.portfolio_name = portfolio_name;
+        self
+    }
+
+    pub fn build(self) -> PortfolioProto {
+        PortfolioProto {
+            object_class: self.object_class,
+            version: self.version,
+            uuid: Some(self.uuid),
+            as_of: Some(self.as_of),
+            is_link: self.is_link,
+            portfolio_name: self.portfolio_name,
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -40,5 +110,22 @@ mod test {
         };
 
         assert_eq!(portfolio.portfolio_name(), "Dummy Name");
+    }
+
+    #[test]
+    fn test_portfolio_builder() {
+        let proto = PortfolioProtoBuilder::new()
+            .portfolio_name("Portfolio".to_string())
+            .build();
+
+        assert!(proto.portfolio_name.contains("Portfolio"));
+
+        let proto2 = PortfolioProtoBuilder::new()
+            .build();
+
+        //Check it's 36 chars long and has a hyphen (i.e. its the UUID)
+        assert!(proto2.portfolio_name.contains("-"));
+        assert_eq!(36, proto2.portfolio_name.len())
+
     }
 }
