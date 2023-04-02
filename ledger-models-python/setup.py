3eaa313fed5590de9eefe6948157aa58c5d78e30
davidjdoherty <dado0583@gmail.com>
2023-04-02 14:39:47 -0400

    Moving from GH action to specifc commands

--- ledger-models-python/setup.py
+++ ledger-models-python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "fintekkers-ledger-models",
-    version='0.0.6',
+    version='0.0.7',
     license='MIT',
     author="David Doherty",
     author_email='davidjdoherty@gmail.com.com',
