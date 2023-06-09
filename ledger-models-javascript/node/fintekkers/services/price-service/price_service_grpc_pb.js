e9e2b8553d939645d7c0b647fe1c48ed45d6f98f
davidjdoherty <dado0583@gmail.com>
2023-06-08 20:47:46 -0400

    renaming price service

---
+++ ledger-models-javascript/node/fintekkers/services/price-service/price_service_grpc_pb.js
@@ -0,0 +1,136 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+'use strict';
+var grpc = require('@grpc/grpc-js');
+var fintekkers_requests_price_query_price_request_pb = require('../../../fintekkers/requests/price/query_price_request_pb.js');
+var fintekkers_requests_price_query_price_response_pb = require('../../../fintekkers/requests/price/query_price_response_pb.js');
+var fintekkers_requests_price_create_price_request_pb = require('../../../fintekkers/requests/price/create_price_request_pb.js');
+var fintekkers_requests_price_create_price_response_pb = require('../../../fintekkers/requests/price/create_price_response_pb.js');
+var fintekkers_requests_util_errors_summary_pb = require('../../../fintekkers/requests/util/errors/summary_pb.js');
+
+function serialize_fintekkers_requests_price_CreatePriceRequestProto(arg) {
+  if (!(arg instanceof fintekkers_requests_price_create_price_request_pb.CreatePriceRequestProto)) {
+    throw new Error('Expected argument of type fintekkers.requests.price.CreatePriceRequestProto');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_fintekkers_requests_price_CreatePriceRequestProto(buffer_arg) {
+  return fintekkers_requests_price_create_price_request_pb.CreatePriceRequestProto.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_fintekkers_requests_price_CreatePriceResponseProto(arg) {
+  if (!(arg instanceof fintekkers_requests_price_create_price_response_pb.CreatePriceResponseProto)) {
+    throw new Error('Expected argument of type fintekkers.requests.price.CreatePriceResponseProto');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_fintekkers_requests_price_CreatePriceResponseProto(buffer_arg) {
+  return fintekkers_requests_price_create_price_response_pb.CreatePriceResponseProto.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_fintekkers_requests_price_QueryPriceRequestProto(arg) {
+  if (!(arg instanceof fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto)) {
+    throw new Error('Expected argument of type fintekkers.requests.price.QueryPriceRequestProto');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_fintekkers_requests_price_QueryPriceRequestProto(buffer_arg) {
+  return fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_fintekkers_requests_price_QueryPriceResponseProto(arg) {
+  if (!(arg instanceof fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto)) {
+    throw new Error('Expected argument of type fintekkers.requests.price.QueryPriceResponseProto');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_fintekkers_requests_price_QueryPriceResponseProto(buffer_arg) {
+  return fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_fintekkers_requests_util_errors_SummaryProto(arg) {
+  if (!(arg instanceof fintekkers_requests_util_errors_summary_pb.SummaryProto)) {
+    throw new Error('Expected argument of type fintekkers.requests.util.errors.SummaryProto');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_fintekkers_requests_util_errors_SummaryProto(buffer_arg) {
+  return fintekkers_requests_util_errors_summary_pb.SummaryProto.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+var SecurityService = exports.SecurityService = {
+  createOrUpdate: {
+    path: '/fintekkers.services.price_service.Security/CreateOrUpdate',
+    requestStream: false,
+    responseStream: false,
+    requestType: fintekkers_requests_price_create_price_request_pb.CreatePriceRequestProto,
+    responseType: fintekkers_requests_price_create_price_response_pb.CreatePriceResponseProto,
+    requestSerialize: serialize_fintekkers_requests_price_CreatePriceRequestProto,
+    requestDeserialize: deserialize_fintekkers_requests_price_CreatePriceRequestProto,
+    responseSerialize: serialize_fintekkers_requests_price_CreatePriceResponseProto,
+    responseDeserialize: deserialize_fintekkers_requests_price_CreatePriceResponseProto,
+  },
+  getByIDs: {
+    path: '/fintekkers.services.price_service.Security/GetByIDs',
+    requestStream: false,
+    responseStream: false,
+    requestType: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    responseType: fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto,
+    requestSerialize: serialize_fintekkers_requests_price_QueryPriceRequestProto,
+    requestDeserialize: deserialize_fintekkers_requests_price_QueryPriceRequestProto,
+    responseSerialize: serialize_fintekkers_requests_price_QueryPriceResponseProto,
+    responseDeserialize: deserialize_fintekkers_requests_price_QueryPriceResponseProto,
+  },
+  search: {
+    path: '/fintekkers.services.price_service.Security/Search',
+    requestStream: false,
+    responseStream: true,
+    requestType: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    responseType: fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto,
+    requestSerialize: serialize_fintekkers_requests_price_QueryPriceRequestProto,
+    requestDeserialize: deserialize_fintekkers_requests_price_QueryPriceRequestProto,
+    responseSerialize: serialize_fintekkers_requests_price_QueryPriceResponseProto,
+    responseDeserialize: deserialize_fintekkers_requests_price_QueryPriceResponseProto,
+  },
+  listIDs: {
+    path: '/fintekkers.services.price_service.Security/ListIDs',
+    requestStream: false,
+    responseStream: false,
+    requestType: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    responseType: fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto,
+    requestSerialize: serialize_fintekkers_requests_price_QueryPriceRequestProto,
+    requestDeserialize: deserialize_fintekkers_requests_price_QueryPriceRequestProto,
+    responseSerialize: serialize_fintekkers_requests_price_QueryPriceResponseProto,
+    responseDeserialize: deserialize_fintekkers_requests_price_QueryPriceResponseProto,
+  },
+  validateCreateOrUpdate: {
+    path: '/fintekkers.services.price_service.Security/ValidateCreateOrUpdate',
+    requestStream: false,
+    responseStream: false,
+    requestType: fintekkers_requests_price_create_price_request_pb.CreatePriceRequestProto,
+    responseType: fintekkers_requests_util_errors_summary_pb.SummaryProto,
+    requestSerialize: serialize_fintekkers_requests_price_CreatePriceRequestProto,
+    requestDeserialize: deserialize_fintekkers_requests_price_CreatePriceRequestProto,
+    responseSerialize: serialize_fintekkers_requests_util_errors_SummaryProto,
+    responseDeserialize: deserialize_fintekkers_requests_util_errors_SummaryProto,
+  },
+  validateQueryRequest: {
+    path: '/fintekkers.services.price_service.Security/ValidateQueryRequest',
+    requestStream: false,
+    responseStream: false,
+    requestType: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    responseType: fintekkers_requests_util_errors_summary_pb.SummaryProto,
+    requestSerialize: serialize_fintekkers_requests_price_QueryPriceRequestProto,
+    requestDeserialize: deserialize_fintekkers_requests_price_QueryPriceRequestProto,
+    responseSerialize: serialize_fintekkers_requests_util_errors_SummaryProto,
+    responseDeserialize: deserialize_fintekkers_requests_util_errors_SummaryProto,
+  },
+};
+
+exports.SecurityClient = grpc.makeGenericClientConstructor(SecurityService);
---
+++ ledger-models-javascript/web/fintekkers/services/price-service/price_service_grpc_web_pb.d.ts
@@ -0,0 +1,93 @@
+import * as grpcWeb from 'grpc-web';
+
+import * as fintekkers_requests_price_create_price_request_pb from '../../../fintekkers/requests/price/create_price_request_pb';
+import * as fintekkers_requests_price_create_price_response_pb from '../../../fintekkers/requests/price/create_price_response_pb';
+import * as fintekkers_requests_price_query_price_request_pb from '../../../fintekkers/requests/price/query_price_request_pb';
+import * as fintekkers_requests_price_query_price_response_pb from '../../../fintekkers/requests/price/query_price_response_pb';
+import * as fintekkers_requests_util_errors_summary_pb from '../../../fintekkers/requests/util/errors/summary_pb';
+
+
+export class SecurityClient {
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: any; });
+
+  createOrUpdate(
+    request: fintekkers_requests_price_create_price_request_pb.CreatePriceRequestProto,
+    metadata: grpcWeb.Metadata | undefined,
+    callback: (err: grpcWeb.RpcError,
+               response: fintekkers_requests_price_create_price_response_pb.CreatePriceResponseProto) => void
+  ): grpcWeb.ClientReadableStream<fintekkers_requests_price_create_price_response_pb.CreatePriceResponseProto>;
+
+  getByIDs(
+    request: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    metadata: grpcWeb.Metadata | undefined,
+    callback: (err: grpcWeb.RpcError,
+               response: fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto) => void
+  ): grpcWeb.ClientReadableStream<fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto>;
+
+  search(
+    request: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    metadata?: grpcWeb.Metadata
+  ): grpcWeb.ClientReadableStream<fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto>;
+
+  listIDs(
+    request: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    metadata: grpcWeb.Metadata | undefined,
+    callback: (err: grpcWeb.RpcError,
+               response: fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto) => void
+  ): grpcWeb.ClientReadableStream<fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto>;
+
+  validateCreateOrUpdate(
+    request: fintekkers_requests_price_create_price_request_pb.CreatePriceRequestProto,
+    metadata: grpcWeb.Metadata | undefined,
+    callback: (err: grpcWeb.RpcError,
+               response: fintekkers_requests_util_errors_summary_pb.SummaryProto) => void
+  ): grpcWeb.ClientReadableStream<fintekkers_requests_util_errors_summary_pb.SummaryProto>;
+
+  validateQueryRequest(
+    request: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    metadata: grpcWeb.Metadata | undefined,
+    callback: (err: grpcWeb.RpcError,
+               response: fintekkers_requests_util_errors_summary_pb.SummaryProto) => void
+  ): grpcWeb.ClientReadableStream<fintekkers_requests_util_errors_summary_pb.SummaryProto>;
+
+}
+
+export class SecurityPromiseClient {
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: any; });
+
+  createOrUpdate(
+    request: fintekkers_requests_price_create_price_request_pb.CreatePriceRequestProto,
+    metadata?: grpcWeb.Metadata
+  ): Promise<fintekkers_requests_price_create_price_response_pb.CreatePriceResponseProto>;
+
+  getByIDs(
+    request: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    metadata?: grpcWeb.Metadata
+  ): Promise<fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto>;
+
+  search(
+    request: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    metadata?: grpcWeb.Metadata
+  ): grpcWeb.ClientReadableStream<fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto>;
+
+  listIDs(
+    request: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    metadata?: grpcWeb.Metadata
+  ): Promise<fintekkers_requests_price_query_price_response_pb.QueryPriceResponseProto>;
+
+  validateCreateOrUpdate(
+    request: fintekkers_requests_price_create_price_request_pb.CreatePriceRequestProto,
+    metadata?: grpcWeb.Metadata
+  ): Promise<fintekkers_requests_util_errors_summary_pb.SummaryProto>;
+
+  validateQueryRequest(
+    request: fintekkers_requests_price_query_price_request_pb.QueryPriceRequestProto,
+    metadata?: grpcWeb.Metadata
+  ): Promise<fintekkers_requests_util_errors_summary_pb.SummaryProto>;
+
+}
+
