39505e00fec0e3b9e09a8f7da36b0fe3e9e3a110
davidjdoherty <dado0583@gmail.com>
2023-07-14 21:53:15 -0400

    Setting package to commonjs

--- ledger-models-javascript/index.js
+++ ledger-models-javascript/index.js
@@ -1,4 +1,4 @@
 
-import * as portfolio from './node/fintekkers/models/portfolio/portfolio_pb.js'
+// import * as portfolio from './node/fintekkers/models/portfolio/portfolio_pb.js'
 
-portfolio.PortfolioProto()
+// portfolio.PortfolioProto()
