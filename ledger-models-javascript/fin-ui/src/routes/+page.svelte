8bce950b380ad68a487bc96bcd53525f71a84470
Deo G <66809588+921Mdas@users.noreply.github.com>
2023-08-01 16:26:01 -0400

    initial commit

---
+++ ledger-models-javascript/fin-ui/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+
+<script >
+	import Login from '../components/Login.svelte'
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<h1>Welcome to Fintekk</h1>
+	<Login />
+</div>
