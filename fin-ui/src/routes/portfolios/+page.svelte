df6ecc579b2b9ef2df48d1b8b1c17bcc0585dd38
davidjdoherty <davidjdoherty@gmail.com>
2023-08-10 09:32:37 -0400

    Adding portfolio page

---
+++ fin-ui/src/routes/portfolios/+page.svelte
@@ -0,0 +1,19 @@
+{@debug}
+<script lang="ts">
+	import { Tile } from 'carbon-components-svelte';
+	import PortfolioGrid from '../../components/PortfolioGrid.svelte';
+
+	export let data: import('./$types').PageData;
+</script>
+
+<div>
+	<Tile>
+		<PortfolioGrid rows={data.portfolioData} />
+	</Tile>
+</div>
+
+<style>
+	:global(:root) {
+		--grid-height: 75vh;
+	}
+</style>
