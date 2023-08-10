17b5fc7b0dd776aee34b9e78b502bb741c8eae60
davidjdoherty <dado0583@gmail.com>
2023-08-10 09:32:37 -0400

    Adding portfolio page

---
+++ fin-ui/src/components/PortfolioGrid.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import {
+		DataTable,
+		Toolbar,
+		ToolbarContent,
+		ToolbarSearch,
+		Button,
+		Pagination
+	} from 'carbon-components-svelte';
+
+	export let rows: Array<{ portfolioName:string; portfolioId:string }>; //: string; portfolioId: string | undefined; id: string }>;
+</script>
+
+<div class="container mx-auto my-6 shadow px-10 py-7">
+	<h2 class="text-3xl font-extrabold my-3">Portfolios</h2>
+	<table class="min-w-full text-left">
+		<thead class="border-b border-slate-400">
+			<tr>
+				<th class="text-semibold text-lg"> Portfolio </th>
+				<th class="text-semibold text-lg">ID</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each rows as row}
+				<tr class="table-row border-b border-slate-400">
+					<td class="table-cell">{row.portfolioName}</td>
+					<td class="table-cell">{row.portfolioId}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
