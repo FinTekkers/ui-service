f5466f77cac133579de4b46bcfa2effc671ed730
davidjdoherty <davidjdoherty@gmail.com>
2023-11-21 15:12:32 -0500

    Adding portfolios to http://localhost:5173/portfolios

---
+++ fin-ui/.svelte-kit/output/server/nodes/7.js
@@ -0,0 +1,8 @@
+import * as server from '../entries/pages/security/_id_/_page.server.ts.js';
+
+export const index = 7;
+export { server };
+export const server_id = "src/routes/security/[id]/+page.server.ts";
+export const imports = [];
+export const stylesheets = [];
+export const fonts = [];
