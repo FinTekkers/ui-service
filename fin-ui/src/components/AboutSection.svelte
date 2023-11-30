b1933eb9a964bcff79b725beffcb636643812176
Deo G <66809588+921Mdas@users.noreply.github.com>
2023-11-29 21:10:16 -0500

    micro interactions landing page

--- fin-ui/src/components/AboutSection.svelte
+++ fin-ui/src/components/AboutSection.svelte
@@ -1,22 +1,44 @@
 <script lang="ts">
+  import Icon from "@iconify/svelte";
+  import {reveal} from 'svelte-reveal';
 
 </script>
 
 <div class="About_section">
-    <div class="abt_section abt_section_1">
-        <h2>Fast</h2>
+    <div class="abt_section abt_section_1" use:reveal={{transition:"fly", y:10}}>
+        <div class="abt_subsectionTitle">
+            <Icon
+            icon="icon-park-outline:speed"
+            style="width: 25px; height: 25px;"          
+            />
+            <h2>Fast</h2>
+
+        </div>
         <p>
          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur mollitia incidunt sequi quaerat enim eum deleniti! Deserunt culpa modi maiores?
         </p>
     </div>
-    <div class="abt_section abt_section_2">
-         <h2>Flexible</h2>
+    <div class="abt_section abt_section_2" use:reveal={{transition:"fly", y:20, delay:20}}>
+         <div class="abt_subsectionTitle">
+            <Icon
+            icon="game-icons:flexible-lamp"
+            style="width: 25px; height: 25px;"          
+            />
+            <h2>Flexible</h2>
+        </div>
         <p>
          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur mollitia incidunt sequi quaerat enim eum deleniti! Deserunt culpa modi maiores?
         </p>
     </div>
-    <div class="abt_section abt_section_3">
-         <h2>Accurate</h2>
+    <div class="abt_section abt_section_3" use:reveal={{transition:"fly", y:30, delay:30}}>
+         <div class="abt_subsectionTitle">
+            <Icon
+            icon="octicon:goal-24"
+            style="width: 25px; height: 25px;"          
+            />
+           <h2>Accurate</h2>
+        </div>
+
         <p>
          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur mollitia incidunt sequi quaerat enim eum deleniti! Deserunt culpa modi maiores?
         </p>
@@ -34,6 +56,12 @@
 
       .abt_section{
         @include flex(column, center, center, 1.5em);
+
+
+        .abt_subsectionTitle{
+        @include flex(row, center, center, .5em);
+
+        }
 
           &:nth-child(n){
               width: 25vw;
--- fin-ui/src/components/VideoSection.svelte
+++ fin-ui/src/components/VideoSection.svelte
@@ -5,8 +5,13 @@
 <div class="Video_section">
 
     <div class="video_description">
-        <h1>Api calls</h1>
+        <h1>Learn More About Fintekkers</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non neque voluptatem ut qui quibusdam tempore placeat dolor. Veniam, iusto ab!</p>
+    </div>
+
+    <div class="video">
+        <iframe width="670" height="380" src="https://www.youtube.com/embed/gjBxFPf0DTs?si=4HlILChW7lLOrpZp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  
     </div>
 
 </div>
@@ -14,17 +19,37 @@
 <style lang="scss">
     @import "../style.scss";
 
+    .video{
+        position: relative;
+        left: 0.6%;
+    }
+
     .Video_section{
        padding: 4em 1em 1em 1em;
-       height: 50vh;
+       height: 100vh;
        background-color: $background-color;
+    //    background-image: url('https://images.unsplash.com/photo-1597673030062-0a0f1a801a31?q=80&w=2796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+       background-image: url('https://images.unsplash.com/photo-1527443154391-507e9dc6c5cc?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+       background-position: center -90px;
+       background-size: cover;
+       background-repeat: no-repeat;
        border: 1px solid #000;
-       @include flex(row,center, flex-start,1em);
+       @include flex(column,flex-start, center,2em);
+       text-align: center;
+
+
+       .video_description{
+       @include flex(column,center, center,1em);
+
+       }
        h1{
-           padding-bottom: 1em;
+           font-size: 2rem;
+           color: $background-color;
        }
        p{
-           width:30vw
+           width:50vw;
+           color: $background-color;
+
        }
 
 
