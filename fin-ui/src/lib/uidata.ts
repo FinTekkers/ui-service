08669d7624eb1ff299b1e35d4a3af7e9b58081a7
Deo G <66809588+921Mdas@users.noreply.github.com>
2023-12-20 17:27:34 -0500

    codeblock adjustment toggling

--- fin-ui/src/lib/uidata.ts
+++ fin-ui/src/lib/uidata.ts
@@ -12,3 +12,71 @@ export const userArchetypes :Obr.userArchetypes[] = [{
     link:"Engineer landing page",
     type:"Engineer"
 }] 
+
+export const installCodeLang : Obr.codeBlockData[]=[{
+    codeLanguage:{
+            language:"Typescript",
+            installCMD:" npm i @fintekkers/ledger-models",
+            importCode:`
+// Model Utils
+    import { FieldProto } from '../../../fintekkers/models/position/field_pb';
+    import * as uuid from '../../models/utils/uuid';
+    import * as dt from '../../models/utils/datetime';
+         
+//Requests & Services
+    import { PortfolioService } from './PortfolioService';
+
+    const now = dt.ZonedDateTime.now();
+
+    const portfolioService = new PortfolioService();
+
+    var searchResults = await portfolioService.searchPortfolio(now.toProto(), 
+    new PositionFilter().addEqualsFilter(FieldProto.PORTFOLIO_NAME, 
+    'Federal Reserve SOMA Holdings'));
+    console.log(searchResults[0].getPortfolioName());
+               `
+        },
+    
+}, {codeLanguage:{
+            language:"Rust",
+            installCMD:" rst i @fintekkers/ledger-models",
+            importCode:`
+// Model Utils
+    import { FieldProto } from '../../../fintekkers/models/position/field_pb';
+    import * as uuid from '../../models/utils/uuid';
+    import * as dt from '../../models/utils/datetime';
+         
+//Requests & Services
+    import { PortfolioService } from './PortfolioService';
+
+    const now = dt.ZonedDateTime.now();
+
+    const portfolioService = new PortfolioService();
+
+    var searchResults = await portfolioService.searchPortfolio(now.toProto(), 
+    new PositionFilter().addEqualsFilter(FieldProto.PORTFOLIO_NAME, 
+    'Federal Reserve SOMA Holdings'));
+    console.log(searchResults[0].getPortfolioName());
+               `
+        }},{codeLanguage:{
+            language:"Java",
+            installCMD:" java i @fintekkers/ledger-models",
+            importCode:`
+// Model Utils
+    import { FieldProto } from '../../../fintekkers/models/position/field_pb';
+    import * as uuid from '../../models/utils/uuid';
+    import * as dt from '../../models/utils/datetime';
+         
+//Requests & Services
+    import { PortfolioService } from './PortfolioService';
+
+    const now = dt.ZonedDateTime.now();
+
+    const portfolioService = new PortfolioService();
+
+    var searchResults = await portfolioService.searchPortfolio(now.toProto(), 
+    new PositionFilter().addEqualsFilter(FieldProto.PORTFOLIO_NAME, 
+    'Federal Reserve SOMA Holdings'));
+    console.log(searchResults[0].getPortfolioName());
+               `
+        }}]
