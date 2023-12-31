76bd8773dc2425da4a9412fc62d7cf4f1afd37c4
Deo G <66809588+921Mdas@users.noreply.github.com>
2023-12-30 21:03:10 -0500

    contact page created and responsiveness adjusted

--- fin-ui/src/components/DashboardSideBar.svelte
+++ fin-ui/src/components/DashboardSideBar.svelte
@@ -11,7 +11,7 @@
     selectedDashboardMenu.set(item);
   };
 </script>
-
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="w-1/4 p-5 flex flex-col gap-20 relative dashboard-sidebar">
   <div
     class="p-2 user-menu cursor-pointer"
@@ -58,7 +58,7 @@
     <span>Account</span>
   </div>
   <div
-    class="absolute user-menu-logout bottom-20 mx-w-1xl p-2 flex gap-4 cursor-pointer"
+    class=" user-menu-logout user-menu cursor-pointer"
     on:click={() => {
         goto("/");
       
@@ -78,16 +78,22 @@
   @import "../style.scss";
 
 .dashboard-sidebar {
-    padding: 2em 0 0 2em;
     background-color: $tealwhite;
     box-shadow: 2px 2px 10px rgba(206, 206, 206, 0.034);
     width: 20vw;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(5,50px);
+    justify-content: center;
+    align-items: center;
+    justify-items: start;
+    padding-top: 2em;
+    
 
           .user-menu {
-            display: flex;
-            position: relative;
-            gap: 10px;
             color: $primary-color;
+            @include flex(row,center, center, 1em);
+            margin: 2em 1em 0 3em;
           }
 
           .user-menu-logout {
@@ -96,18 +102,29 @@
 
           .user-menu-icon {
             vertical-align: baseline;
-            font-size: 25px;
-            position: absolute;
           }
   }
 
- 
+@media screen and (max-width: $breakingpoint_medium){
+  .dashboard-sidebar{
+    width: 35vw;
+  }
+}
 
 @media screen and (max-width: $breakingpoint_mobile){
 
   .dashboard-sidebar{
-    padding-left: 1em;
+    display: flex;
+    align-items: center;
+
+   .user-menu-logout{
+    padding: 0;
+   }
+
     div{
+      width: 100%;
+      margin-right: 1em;
+
       span{
         display: none;
       }
