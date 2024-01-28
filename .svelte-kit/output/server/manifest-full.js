8d91f82d2698adc96cec2bd1de6b0592659c8429
Deo G <66809588+921Mdas@users.noreply.github.com>
2024-01-28 11:20:49 -0500

    deleted subfolder fin-ui that contained another src folder

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".DS_Store","favicon.png","images/.DS_Store"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.d6c319ae.js","app":"_app/immutable/entry/app.902983d6.js","imports":["_app/immutable/entry/start.d6c319ae.js","_app/immutable/chunks/index.225eb311.js","_app/immutable/chunks/singletons.271e1aeb.js","_app/immutable/chunks/index.5675a6da.js","_app/immutable/entry/app.902983d6.js","_app/immutable/chunks/index.225eb311.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.fb800289.js","app":"_app/immutable/entry/app.487c458b.js","imports":["_app/immutable/entry/start.fb800289.js","_app/immutable/chunks/index.225eb311.js","_app/immutable/chunks/singletons.1a15a3bc.js","_app/immutable/chunks/index.5675a6da.js","_app/immutable/entry/app.487c458b.js","_app/immutable/chunks/index.225eb311.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- fin-ui/src/lib/Util.ts
+++
@@ -1,7 +0,0 @@
-export enum dashboardMenuList {
-  Home = "home",
-  Dashboard = "Dashboard",
-  Portfolio = "Portfolio",
-  Account = "Account",
-  Logout = "Logout",
-}
