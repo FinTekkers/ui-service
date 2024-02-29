273619ee3e99d366390b5cdfbb721ef65f636ccb
davidjdoherty <dado0583@gmail.com>
2024-02-29 15:45:46 -0400

    Removing .sveltekit

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.69449468.js","_app/immutable/chunks/index.225eb311.js","_app/immutable/chunks/index.5675a6da.js","_app/immutable/chunks/store.d6707496.js","_app/immutable/chunks/singletons.6ee9b6c7.js","_app/immutable/chunks/helper.0e794cd8.js"];
-export const stylesheets = ["_app/immutable/assets/0.cccac512.css"];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/login/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.5972cb2c.js","_app/immutable/chunks/index.225eb311.js"];
-export const stylesheets = [];
-export const fonts = [];
