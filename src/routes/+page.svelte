8625d4434dde1498f2709d32b2a0737b54184584
davidjdoherty <davidjdoherty@gmail.com>
2024-03-04 19:49:28 -0500

    Commenting out graph section because it wasn't working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import LandingOverview from "../components/LandingSection.svelte";
   import AboutSection from "../components/OurFeatureSection.svelte";
   import VideoSection from "../components/VideoSection.svelte";
-  import GraphSection from "../components/GraphSection.svelte";
+  // import GraphSection from "../components/GraphSection.svelte";
 
   export let data: import("./$types").PageData;
 </script>
@@ -12,7 +12,7 @@
 <div class="landing_page_overlay">
   <LandingOverview />
   <LandingSection />
-  <GraphSection />
+  <!-- <GraphSection /> -->
   <AboutSection />
   <VideoSection />
   <Footer />
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,30 +1,26 @@
 import { writable, get } from "svelte/store";
 import type { booleanStoreType, portfolioStoreType } from "../lib/types";
-import  { booleanKeys } from "$lib/Util";
-
+import { booleanKeys } from "$lib/Util";
 
 // store updating all project booleans
 export const booleanStore = writable<booleanStoreType>({
- [booleanKeys.IS_SIDE_NAV_ACTIVE]: false,
- [booleanKeys.IS_OBR_PROMPT_SHOWING]:true,
- [booleanKeys.IS_SIGN_IN_OR_SIGN_UP]:false,
- [booleanKeys.IS_PASSWORD_VISIBLE]:false,
- [booleanKeys.IS_FEATURE_ACTIVE]:false,
+  [booleanKeys.IS_SIDE_NAV_ACTIVE]: false,
+  [booleanKeys.IS_OBR_PROMPT_SHOWING]: true,
+  [booleanKeys.IS_SIGN_IN_OR_SIGN_UP]: false,
+  [booleanKeys.IS_PASSWORD_VISIBLE]: false,
+  [booleanKeys.IS_FEATURE_ACTIVE]: false,
 })
 
 // function that updates the boolean store, receive a key to target specific boolean
-export const customBooleanStoreUpdater:(key:booleanKeys)=>void = (key: booleanKeys)=>{
+export const customBooleanStoreUpdater: (key: booleanKeys) => void = (key: booleanKeys) => {
   let _storeValue = get(booleanStore);
-  if(typeof key === 'string'){ 
-    booleanStore.update((store)=>{
+  if (typeof key === 'string') {
+    booleanStore.update((store) => {
       store[key] = !_storeValue[key]
       return store
- })
+    })
 
-}
-
-
-
+  }
 }
 
 // store importing the api content to populate the dashboards #number of calls, #keys, etc.;
@@ -50,8 +46,8 @@ export const portfolioStoreUpdater = async (data: App.PageData) => {
 export const selectedDashboardMenu = writable<string>("home");
 
 export const selectedDashboardMenuUpdater = (item: string) => {
-    selectedDashboardMenu.set(item);
-  };
+  selectedDashboardMenu.set(item);
+};
 
 
 
