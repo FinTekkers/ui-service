c463c1987cd7ed1bd9a317c4bc25bcd862d27a98
davidjdoherty <dado0583@gmail.com>
2024-03-04 19:49:28 -0500

    Commenting out graph section because it wasn't working

--- src/routes/portfolios/+page.server.ts
+++ src/routes/portfolios/+page.server.ts
@@ -8,7 +8,6 @@ import type Portfolio from "@fintekkers/ledger-models/node/wrappers/models/portf
 import pkg from '@fintekkers/ledger-models/node/fintekkers/models/position/field_pb.js';
 const { FieldProto } = pkg;
 
-
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
   const now = dt.ZonedDateTime.now();
