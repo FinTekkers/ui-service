a8eb4a938f0c9c86ad4388354a34ab05a13f2e26
feanyluch <ifeanyioluchi7@gmail.com>
2024-03-12 21:42:39 +0100

    commiting local changes

--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -1,50 +1,64 @@
 <!-- routes/Data.svelte -->
 <script lang="ts">
-    import Portfolio from "../../components/widgets/PortfolioGrid.svelte";
-    import Security from "../../components/widgets/SecurityGrid.svelte";
-    import Transaction from "../../components/widgets/TransactionGrid.svelte";
-    import DashboardSidebar from "../../components/DashboardSideBar.svelte";
-    import { dashboardMenuList } from "$lib/Util";
-    import { selectedDashboardMenu } from "../../store/store";
-    export let data: import("./$types").PageData;
+  import Portfolio from "../../components/widgets/PortfolioGrid.svelte";
+  import Security from "../../components/widgets/SecurityGrid.svelte";
+  import Transaction from "../../components/widgets/TransactionGrid.svelte";
+  import PositionInput from "../../components/widgets/PositionInput.svelte";
+  import DashboardSidebar from "../../components/DashboardSideBar.svelte";
+  import { dashboardMenuList } from "$lib/Util";
+  import { selectedDashboardMenu } from "../../store/store";
+  export let data: import("./$types").PageData;
 
-    // console.log(data.transactions)
-    selectedDashboardMenu.set(dashboardMenuList.PORTFOLIO);
-  
-    // Function to handle menu selection
-    const handleMenuSelection = (event: CustomEvent<any>) => {
-      const menu: string = event.detail;
-      selectedDashboardMenu.set(menu);
-    };
-  </script>
-  
-  <div class="w-screen h-full flex">
-    <DashboardSidebar on:menuSelect={handleMenuSelection} />
-    <div class="h-full w-screen dashboard-container">
-      {#if $selectedDashboardMenu === dashboardMenuList.PORTFOLIO}
-      <Portfolio rows={Array.isArray(data.portfolios) ? data.portfolios : [data.portfolios]} />
-      {:else if $selectedDashboardMenu === dashboardMenuList.SECURITY}
-        <Security rows={Array.isArray(data.security) ? data.security : [data.security]} />
-        {:else if $selectedDashboardMenu === dashboardMenuList.TRANSACTION}
-        <Transaction rows={Array.isArray(data.transactions) ? data.transactions : [data.transactions]} />
-      {/if}
-    </div>
+  console.log(data.positions)
+  selectedDashboardMenu.set(dashboardMenuList.PORTFOLIO);
+
+  // Function to handle menu selection
+  const handleMenuSelection = (event: CustomEvent<any>) => {
+    const menu: string = event.detail;
+    selectedDashboardMenu.set(menu);
+  };
+</script>
+
+<div class="w-screen h-full flex">
+  <DashboardSidebar on:menuSelect={handleMenuSelection} />
+  <div class="h-full w-screen dashboard-container">
+    {#if $selectedDashboardMenu === dashboardMenuList.PORTFOLIO}
+      <Portfolio
+        rows={Array.isArray(data.portfolios)
+          ? data.portfolios
+          : [data.portfolios]}
+      />
+    {:else if $selectedDashboardMenu === dashboardMenuList.SECURITY}
+      <Security
+        rows={Array.isArray(data.security) ? data.security : [data.security]}
+      />
+    {:else if $selectedDashboardMenu === dashboardMenuList.TRANSACTION}
+      <Transaction
+        rows={Array.isArray(data.transactions)
+          ? data.transactions
+          : [data.transactions]}
+      />
+    {:else if $selectedDashboardMenu === dashboardMenuList.POSITION}
+      <!-- Change to Positions menu item -->
+      <PositionInput />
+    {/if}
   </div>
-  
-  <style lang="scss">
-    @import "../../style.scss";
-  
-    .dashboard-container {
-      background-color: white;
-      @include flex(column, center, center, 0);
-  
-      .dashboard-menu {
-        width: 98%;
-        height: 98%;
-        padding: 2em;
-        border-radius: 5px;
-        background-color: $tealblack;
-        color: $primary-color;
-      }
+</div>
+
+<style lang="scss">
+  @import "../../style.scss";
+
+  .dashboard-container {
+    background-color: white;
+    @include flex(column, center, center, 0);
+
+    .dashboard-menu {
+      width: 98%;
+      height: 98%;
+      padding: 2em;
+      border-radius: 5px;
+      background-color: $tealblack;
+      color: $primary-color;
     }
-  </style>
+  }
+</style>
