5718a23964845a084a336de5910aa363dc972bcd
davidjdoherty <dado0583@gmail.com>
2025-03-26 10:11:38 -0400

    Removing github oauth

--- src/routes/contactus/+page.server.ts
+++ src/routes/contactus/+page.server.ts
@@ -1,4 +1,4 @@
-import {  redirect } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
 import * as Yup from 'yup';
 import { isValidationError } from "$lib/helper";
 
@@ -9,47 +9,49 @@ import { isValidationError } from "$lib/helper";
 const validationSchema = Yup.object({
     firstname: Yup.string().required('firstname'),
     lastname: Yup.string().required('lastname'),
-    email:Yup.string().required('email').email('Invalid email'),
-    message:Yup.string().required('message'),
+    email: Yup.string().required('email').email('Invalid email'),
+    message: Yup.string().required('message'),
 
 })
 
-let firstname:FormDataEntryValue | null, lastname:FormDataEntryValue | null, email:FormDataEntryValue | null, message: FormDataEntryValue | null;
+let firstname: FormDataEntryValue | null, lastname: FormDataEntryValue | null, email: FormDataEntryValue | null, message: FormDataEntryValue | null;
 
 export const actions = {
-    message: async ({request}:{request:Request})=>{
-        let isValid: any = null ;
-        let formError : string[] | null = null;;
-       try{
+    message: async ({ request }: { request: Request }) => {
+        console.log("Will attempt to send mail");
+        let isValid: any = null;
+        let formError: string[] | null = null;;
+        try {
 
-                const data = await request.formData();
+            const data = await request.formData();
 
-                firstname= data.get('firstname');
-                lastname= data.get('lastname');
-                email= data.get('email');
-                message= data.get('message');
-               
-                isValid = await validationSchema.validate({firstname, lastname, email, message}, { abortEarly: false });
+            firstname = data.get('firstname');
+            lastname = data.get('lastname');
+            email = data.get('email');
+            message = data.get('message');
 
-        
+            isValid = await validationSchema.validate({ firstname, lastname, email, message }, { abortEarly: false });
 
 
-       }catch(validationError){
 
-             // Validation failed
+
+        } catch (validationError) {
+
+            // Validation failed
             if (isValidationError(validationError)) {
                 const validationErrors = validationError.inner.map((error: { message: string }) => error.message);
                 formError = validationErrors;
-            } 
-
-       }
-
-             // Perform redirection outside the try...catch block if validation succeeded
-            if (isValid && !formError) {
-                throw redirect(303, "/contactus");
             }
 
-            // Return formError if there are any validation errors && form data to persist value / form manual enhacement
-            return { formError, firstname, lastname, email, message, success:true };
+        }
+
+        // Perform redirection outside the try...catch block if validation succeeded
+        if (isValid && !formError) {
+            console.log("TODO: Implement");
+            throw redirect(303, "/contactus");
+        }
+
+        // Return formError if there are any validation errors && form data to persist value / form manual enhacement
+        return { formError, firstname, lastname, email, message, success: true };
     }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import  {sveltekit} from "@sveltejs/kit/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Allow all origins
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS', // Allowed HTTP methods
+      'Access-Control-Allow-Credentials': 'true', // Allow credentials
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization' // Allowed headers
+    }
+  }
 });
