5b75431a41d5d8d285bcb94619558d80816404f4
davidjdoherty <davidjdoherty@gmail.com>
2025-03-29 18:55:38 -0400

    More cleanup

--- src/lib/ROUTES.ts
+++ src/lib/ROUTES.ts
@@ -8,44 +8,44 @@
 /**
  * PAGES
  */
-const PAGES = {
-  "/": `/`,
-  "/auth/email-verification": `/auth/email-verification`,
-  "/auth/login": `/auth/login`,
-  "/auth/register": `/auth/register`,
-  "/auth/reset-password": `/auth/reset-password`,
-  "/dashboard": `/dashboard`
-}
+// const PAGES = {
+//   "/": `/`,
+//   "/auth/email-verification": `/auth/email-verification`,
+//   "/auth/login": `/auth/login`,
+//   "/auth/register": `/auth/register`,
+//   "/auth/reset-password": `/auth/reset-password`,
+//   "/dashboard": `/dashboard`
+// }
 
-/**
- * SERVERS
- */
-const SERVERS = {
-  "GET oauth/google": `oauth/google`,
-  "GET oauth/google/callback": `oauth/google/callback`
-}
+// /**
+//  * SERVERS
+//  */
+// const SERVERS = {
+//   "GET oauth/google": `oauth/google`,
+//   "GET oauth/google/callback": `oauth/google/callback`
+// }
 
-/**
- * ACTIONS
- */
-const ACTIONS = {
-  "verifyCode /auth/email-verification": `/auth/email-verification?/verifyCode`,
-  "sendNewCode /auth/email-verification": `/auth/email-verification?/sendNewCode`,
-  "logInUser /auth/login": `/auth/login?/logInUser`,
-  "sendPasswordResetEmail /auth/login": `/auth/login?/sendPasswordResetEmail`,
-  "registerUser /auth/register": `/auth/register?/registerUser`,
-  "resetPassword /auth/reset-password": `/auth/reset-password?/resetPassword`,
-  "logout /dashboard": `/dashboard?/logout`,
-  "changePassword /dashboard": `/dashboard?/changePassword`,
-  "deleteAllUsers /dashboard": `/dashboard?/deleteAllUsers`
-}
+// /**
+//  * ACTIONS
+//  */
+// const ACTIONS = {
+//   "verifyCode /auth/email-verification": `/auth/email-verification?/verifyCode`,
+//   "sendNewCode /auth/email-verification": `/auth/email-verification?/sendNewCode`,
+//   "logInUser /auth/login": `/auth/login?/logInUser`,
+//   "sendPasswordResetEmail /auth/login": `/auth/login?/sendPasswordResetEmail`,
+//   "registerUser /auth/register": `/auth/register?/registerUser`,
+//   "resetPassword /auth/reset-password": `/auth/reset-password?/resetPassword`,
+//   "logout /dashboard": `/dashboard?/logout`,
+//   "changePassword /dashboard": `/dashboard?/changePassword`,
+//   "deleteAllUsers /dashboard": `/dashboard?/deleteAllUsers`
+// }
 
 // route function helpers
 type NonFunctionKeys<T> = { [K in keyof T]: T[K] extends Function ? never : K }[keyof T]
 type FunctionKeys<T> = { [K in keyof T]: T[K] extends Function ? K : never }[keyof T]
 type FunctionParams<T> = T extends (...args: infer P) => any ? P : never
 
-const AllObjs = { ...PAGES, ...ACTIONS, ...SERVERS }
+const AllObjs = { ...PAGES, /*...ACTIONS, ...SERVERS */}
 type AllTypes = typeof AllObjs
 
 /**
