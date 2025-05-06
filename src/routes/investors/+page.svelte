4a9e79a8e52bd7511a06edb6ba083731a596177f
Deo G <66809588+921Mdas@users.noreply.github.com>
2025-05-06 18:44:41 -0400

    merging investors page

---
+++ src/routes/investors/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  let landingUrl = "https://zingy-cannoli-1eadac.netlify.app/"; // or wherever it's hosted
+</script>
+
+<iframe
+  src={landingUrl}
+  style="width: 100%; height: 100vh; border: none;"
+  title="Landing Page"
+/>
